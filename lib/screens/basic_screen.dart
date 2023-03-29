8c7e83f33a8f8fab2c0dddbb8471d40761237da2
Luis Rivas <luisricardorivasgiwencer@gmail.com>
2023-03-29 16:50:58 -0400

    design finished

--- lib/screens/basic_screen.dart
+++ lib/screens/basic_screen.dart
@@ -7,9 +7,60 @@ class BasicScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
-        const Image(image: AssetImage('assets/land.jpg')),
-        Title()
+        Image(image: AssetImage('assets/land.jpg')),
+        Title(),
+        ButtonSection(),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+        ),
       ]),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            children: const [
+              Icon(
+                Icons.call,
+                color: Colors.blue,
+              ),
+              Text('Call'),
+            ],
+          ),
+          Column(
+            children: const [
+              Icon(
+                Icons.send,
+                color: Colors.blue,
+              ),
+              Text('Send'),
+            ],
+          ),
+          Column(
+            children: const [
+              Icon(
+                Icons.share,
+                color: Colors.blue,
+              ),
+              Text('Share'),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
